b50c6e1a3bb2be61943d4779105f8c37bec4ac87
sai-d-kumar-balu <balusaikumar1233@gmail.com>
2025-06-22 21:23:26 +0530

    changed the user in api gateway

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Api Gateway' });
+  res.render('index', { title: 'Express Api Gateway v.2' });
 });
 
 module.exports = router;
